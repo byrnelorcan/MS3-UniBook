fce1bb6818798d767e8c522bd355295e4d75705e
byrnelorcan <lorcan.byrne@icloud.com>
2021-09-13 19:09:09 +0000

    add user profile page

--- app.py
+++ app.py
@@ -48,6 +48,8 @@ def register():
         # new user goes into the session cookie
         session["user"] = request.form.get("username").lower()
         flash("Awesome! Registration complete!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -61,9 +63,12 @@ def login():
         if existing_user:
             # ensure password is the same
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
-                flash("Welcome, {}".format(request.form.get("username")))
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
                 # incorrect password
                 flash("Oops! Thats the wrong Username or Password!")
@@ -75,6 +80,13 @@ def login():
             return redirect(url_for("login"))
     
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
